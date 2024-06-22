e758afb4daa9d3700c06a04d40a23d091ef4bbd7
hulv-stealth-mode <Hulvdan@yandex.ru>
2024-06-22 17:07:52 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+from glob import glob
+
+from iterfzf import iterfzf
+
+
+paths = [
+    "~/Downloads/",
+    "~/dev/",
+    "~/dev/*/",
+    "~/dev/gdev/*/",
+    "~/dev/home/*/",
+    "~/dev/work/*/",
+    "~/dev/ref/*/",
+]
+
+
+home_path = os.path.expanduser(os.path.normpath("~/"))
+
+all_paths = []
+
+
+for p in paths:
+    p = os.path.normpath(p)
+    p = os.path.expanduser(p)
+    if "*" in p:
+        all_paths.extend(
+            "~" + i.removeprefix(home_path) for i in
+            glob(p, recursive=True, include_hidden=True)
+        )
+    else:
+        p = "~" + p.removeprefix(home_path)
+        all_paths.append(p)
+
+all_paths.sort()
+
+a = iterfzf(all_paths)
+
+if a is not None:
+    directory = os.path.expanduser(a)
+    command = "neovide.exe ."
+    print(directory, command)
+    subprocess.call(command, cwd=directory)
---
+++ run.bat
@@ -0,0 +1,1 @@
+C:\Users\user\dev\home\launcher\.venv\Scripts\python.exe main.py
